5fb50946e92d04da0799aada343b694061d03a1b
VengeanceIsHis <elijahjpoole@gmail.com>
2024-10-02 10:37:16 -0500

    adding danger zone

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
     public Transform player;
     public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-       transform.position = player.position + offset;
+        transform.position = player.position + offset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    private int score = 0;
     public float speed = 5f;
     private Rigidbody rb;
 
@@ -18,12 +19,24 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // WASD buttons enabled
         float direction_x = Input.GetAxis("Horizontal");
         float direction_z = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(direction_x, 0.0f, direction_z).normalized;
 
-
+        // Rigid Body are for collisions
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If Player collides with Tag "Pickup"
+        if (other.CompareTag("Pickup"))
+        {
+            score += 1;
+            Debug.Log("Score: " + score);
+            Destroy(other.gameObject);
+        }
     }
 }
